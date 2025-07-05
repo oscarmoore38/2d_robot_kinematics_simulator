12ad7987be3be2262d4df6bcff2ecef31719940f
Oscar Moore <oscarjmoore38@gmail.com>
2025-07-05 10:11:48 +0000

    Added skeleton code for Pointer class

--- Src/PointerClass/PointerClass.hpp
+++ Src/PointerClass/PointerClass.hpp
@@ -1,11 +1,29 @@
 #ifndef POINTER_HPP
 #define POINTER_HPP
 
+/**
+ * @class Pointer
+ * @brief Represents a 2D coordinate (x, y) in Cartesian space and also handles geometric calculations.
+ * 
+ */
+
 class Pointer{
+
+private:
+    double yCord_; 
+    double xCord_; 
 public:
     Pointer();
 
-private:
+    Pointer(double yCord, double xCord);
+
+    double getXCord_() const; 
+
+    double getYCord_() const; 
+
+    void CalculateIK();
+
+    void CalculateFK(); 
     
 };
 
