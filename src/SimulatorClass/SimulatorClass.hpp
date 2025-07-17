8f30f07896cbd7e48be92866aacc33ed39cb3ded
Oscar Moore <oscarjmoore38@gmail.com>
2025-07-17 09:48:25 +0000

    Added skeleton code. Added and configured websocket dependencies

--- src/SimulatorClass/SimulatorClass.hpp
+++ src/SimulatorClass/SimulatorClass.hpp
@@ -1,11 +1,27 @@
 #ifndef SIMULATOR_HPP
 #define SIMULATOR_HPP
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include "RobotArmClass/RobotArmClass.hpp"
+
+/**
+ * @class Simulator
+ * @brief Manages the GUI, visualizes the RobotArm, and processes user input. 
+ * 
+ */
 
 class Simulator {
 public:
-    Simulator();
+
+    RobotArm newRoboticArm; 
+    websocketpp::server<websocketpp::config::asio> simulatorServer;
 
 private:
+    void startServer(); 
+
+    void stopServer(); 
+
+    void handleMessage(); 
     
 };
 
